37e1f8eb6f45004d82f1b7f9d10e32067b89dff7
cytopz <cytopz@protonmail.com>
2021-01-20 14:34:18 +0700

    return more device details

--- api/adb.py
+++ api/adb.py
@@ -6,6 +6,15 @@ router = APIRouter(prefix='/adb')
 @router.get('/devices')
 def devices():
     devices = Adb.list_devices()
+    if len(devices) == 1:
+        devices = [
+            {
+                'serial': devices[0],
+                'name': Adb.exec_out('getprop ro.product.model').decode('utf-8').strip(),
+                'screen': Adb.exec_out('wm size').decode('utf-8').strip().split(': ')[1],
+            }
+        ]
     return {
         'devices': devices
     }
+
