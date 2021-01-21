544635ed62c9104d0d3bfcc80363e7a1449d95c7
cytopz <cytopz@protonmail.com>
2021-01-21 19:27:39 +0700

    add web interface dist
    Signed-off-by: cytopz <cytopz@protonmail.com>

---
+++ webui/js/chunk-2d0e95df.9cea8290.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h1",[n._v("404")])},u=[],l={name:"404"},a=l,r=t("2877"),s=Object(r["a"])(a,c,u,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e95df.9cea8290.js.map
